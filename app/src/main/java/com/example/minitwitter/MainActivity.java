7485a4f373f97469c0ddf987c7ea6328b6377b26
Javier Islas <javier.i@inspireinnovations.com>
2019-04-24 17:50:56 -0500

    Termino de clase.

--- app/src/main/java/com/example/minitwitter/MainActivity.java
+++ app/src/main/java/com/example/minitwitter/MainActivity.java
@@ -1,13 +1,43 @@
 package com.example.minitwitter;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnLogin;
+    TextView tvgoSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogin = findViewById(R.id.buttonLogin);
+        tvgoSignUp = findViewById(R.id.textViewGoSignUp);
+
+        btnLogin.setOnClickListener(this);
+        tvgoSignUp.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch(id){
+            case R.id.buttonLogin:
+                break;
+            case R.id.textViewGoSignUp:
+                goToSignUp();
+                break;
+        }
+    }
+
+    private void goToSignUp() {
+        Intent i = new Intent(MainActivity.this, SignUpActivity.class);
+        startActivity(i);
     }
 }
