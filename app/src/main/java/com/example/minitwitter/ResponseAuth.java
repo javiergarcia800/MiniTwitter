e7c6aa4d3e7b54a8df61fb824aba2c08a9a0c90c
Javier Islas <javier.i@inspireinnovations.com>
2019-04-26 10:57:17 -0500

    Termina la clase 74

---
+++ app/src/main/java/com/example/minitwitter/ResponseAuth.java
@@ -0,0 +1,91 @@
+package com.example.minitwitter;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class ResponseAuth {
+
+    @SerializedName("token")
+    @Expose
+    private String token;
+
+    @SerializedName("username")
+    @Expose
+    private String username;
+
+    @SerializedName("email")
+    @Expose
+    private String email;
+
+    @SerializedName("photoUrl")
+    @Expose
+    private String photoUrl;
+
+    @SerializedName("created")
+    @Expose
+    private String created;
+
+    @SerializedName("active")
+    @Expose
+    private boolean active;
+
+    public ResponseAuth() {
+    }
+
+    public ResponseAuth(String token, String username, String email, String photoUrl, String created, boolean active) {
+        this.token = token;
+        this.username = username;
+        this.email = email;
+        this.photoUrl = photoUrl;
+        this.created = created;
+        this.active = active;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+
+    public String getCreated() {
+        return created;
+    }
+
+    public void setCreated(String created) {
+        this.created = created;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+}
