c1bd865d217f3b7031ce3a18fb0bf389f6e527a7
Javier Islas <javier.i@inspireinnovations.com>
2019-04-29 10:06:38 -0500

    Termina la clase 78

--- app/src/main/java/com/example/minitwitter/ui/SignUpActivity.java
+++ app/src/main/java/com/example/minitwitter/ui/SignUpActivity.java
@@ -5,14 +5,31 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.minitwitter.R;
+import com.example.minitwitter.retrofit.MiniTwitterClient;
+import com.example.minitwitter.retrofit.MiniTwitterService;
+import com.example.minitwitter.retrofit.request.RequestSignup;
+import com.example.minitwitter.retrofit.response.ResponseAuth;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button btnSignUp;
     TextView tvGoLogin;
+
+    EditText etUsername;
+    EditText etEmail;
+    EditText etPassword;
+
+    MiniTwitterClient miniTwitterClient;
+    MiniTwitterService miniTwitterService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +38,27 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
 
         getSupportActionBar().hide();
 
+        retrofitInit();
+        findViews();
+        events();
+
+    }
+
+    private void retrofitInit() {
+        miniTwitterClient = MiniTwitterClient.getInstance();
+        miniTwitterService = miniTwitterClient.getMiniTwitterService();
+    }
+
+    private void findViews() {
         btnSignUp = findViewById(R.id.buttonSignUp);
         tvGoLogin = findViewById(R.id.textViewGoLogin);
 
+        etUsername = findViewById(R.id.editTextUserName);
+        etEmail = findViewById(R.id.editTextEmail);
+        etPassword = findViewById(R.id.editTextPassword);
+    }
+
+    private void events() {
         btnSignUp.setOnClickListener(this);
         tvGoLogin.setOnClickListener(this);
     }
@@ -33,10 +68,47 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
         int id = v.getId();
         switch(id){
             case R.id.buttonSignUp:
+                goToSignUp();
                 break;
             case R.id.textViewGoLogin:
                 goToLogin();
                 break;
+        }
+    }
+
+    private void goToSignUp() {
+        String username = etUsername.getText().toString();
+        String email = etEmail.getText().toString();
+        String password = etPassword.getText().toString();
+
+        if (username.isEmpty()) {
+            etUsername.setError("El nombre de usuario es requerido");
+        } else  if (email.isEmpty()) {
+            etEmail.setError("El email es requerido");
+        } else if (password.isEmpty() || password.length() < 4) {
+            etPassword.setError("La contraseña es requerida y debe tener al almenos 4 caracteres");
+        } else {
+            String code = "UDEMYANDROID";
+            RequestSignup requestSignup = new RequestSignup(username, email, password, code);
+            Call<ResponseAuth> call = miniTwitterService.doSignup(requestSignup);
+            call.enqueue(new Callback<ResponseAuth>(){
+
+                @Override
+                public void onResponse(Call<ResponseAuth> call, Response<ResponseAuth> response) {
+                    if (response.isSuccessful()) {
+                        Intent i = new Intent(SignUpActivity.this, DashboardActivity.class);
+                        startActivity(i);
+                        finish();
+                    } else {
+                        Toast.makeText(SignUpActivity.this, "Algo ha ido mal, revise los datos", Toast.LENGTH_SHORT);
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<ResponseAuth> call, Throwable t) {
+                    Toast.makeText(SignUpActivity.this, "Error en la conexión, inténtelo de nuevo", Toast.LENGTH_SHORT);
+                }
+            });
         }
     }
 
