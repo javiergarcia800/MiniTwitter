37bcacb5e925fca9446536b0eea855a3503395c7
Javier Islas <javier.i@inspireinnovations.com>
2019-04-30 12:57:31 -0500

    Termina la clase 87

--- app/src/main/java/com/example/minitwitter/ui/DashboardActivity.java
+++ app/src/main/java/com/example/minitwitter/ui/DashboardActivity.java
@@ -5,12 +5,17 @@ import com.example.minitwitter.TweetListFragment;
 
 import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.annotation.NonNull;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.TextView;
 
 public class DashboardActivity extends AppCompatActivity {
+
+    FloatingActionButton fab;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -34,6 +39,8 @@ public class DashboardActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
+        fab = findViewById(R.id.fab);
+
         getSupportActionBar().hide();
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
@@ -45,6 +52,14 @@ public class DashboardActivity extends AppCompatActivity {
                 .add(R.id.fragmentContainer, new TweetListFragment())
                 .commit();
 
+        fab.setOnClickListener(new OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                NuevoTweetDialogFragment dialog = new NuevoTweetDialogFragment();
+                dialog.show(getSupportFragmentManager(), "NuevoTweetDialogFragment");
+            }
+        });
     }
 
 }
